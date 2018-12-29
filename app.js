7eea374cfa2c840f929c6567514a6ff4c34c3f5c
ingriak <ingriak@stud.ntnu.no>
2018-12-29 17:26:25 +0100

    Finished "Introducing Props"
    app.js after finishing the course «Introducing Props»

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+const players = [
+    {
+        name: "Kaspar",
+        score: 50
+      },
+      {
+        name: "Pappa",
+        score: 85
+      },
+      {
+        name: "Ingrid",
+        score: 95
+      },
+      {
+        name: "Mamma",
+        score: 80
+      }
+];
 
 //Create a React component as either a JS function or class
 // React components are written in plain JS, with the help of JSX
-function Header(){
+const Header = (props) => {
     return (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{ props.title }</h1>
+            <span className="stats">Players: { props.totalPlayers }</span>
         </header>
     );
 }
@@ -29,45 +47,53 @@ const header = (
 );
 */
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Ingrid
+                {props.name}
             </span>
 
            {/* Composition: component contains another component */} 
-            <Counter />
-
+            <Counter score={props.score}/>
         </div>
     );
 }
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-                <span className="counter-score">35</span>
+                <span className="counter-score">{props.score}</span>
             <button className="counter-action increment"> + </button>
         </div>
     );
 }
 
 // Ties every component together
-const App = () => {
+const App = (props) => {
     return(
         <div className="scoreboard">
-            <Header />
+            <Header 
+            title="Scoreboard" 
+            totalPlayers={players.length}
+            />
 
-            {/* Playera list */}
-            <Player />
+            {/* Player list */}
+            {props.initialPlayers.map( player => 
+                <Player 
+                name={player.name}
+                score={player.score}
+                />
+            )}
         </div>
     );
 }
 
 //Not before render that browser creates actuall DOM elements
 //Renders job to interpret element objects and create DOM nodes out of them
+// Usally pass the Top-level component to ReactDOM.render()
 ReactDOM.render(
-    <App />,
+    <App initialPlayers={players}/>,
     document.getElementById('root')
 );
 
