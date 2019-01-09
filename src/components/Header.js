cd340efb04f1736797c868c918570da655c39023
ingriak <ingriak@stud.ntnu.no>
2019-01-09 15:10:00 +0100

    Created stopwatch component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Stats from './Stats';
+import Stopwatch from './Stopwatch';
 
 //Create a React component as either a JS function or class
 // React components are written in plain JS, with the help of JSX
@@ -8,6 +9,7 @@ const Header = (props) => {
         <header>
             <Stats players={props.players} />
             <h1>{props.title}</h1>
+            <Stopwatch />
         </header>
     );
 }
